57b9df89ff93d663696689db469481d1063bae9f
Androz <androz2091@gmail.com>
2022-04-30 18:20:32 +0200

    Update stop.ts

--- slash-commands/stop.ts
+++ slash-commands/stop.ts
@@ -17,6 +17,10 @@ export const commands = [
 export const run: SlashCommandRunFunction = async (interaction) => {
 
     const member = interaction.member as GuildMember;
+
+    const status = await getConnection().getRepository(ServerPlayingStatus).findOne({
+        guildId: interaction.guildId!
+    }) ?? new ServerPlayingStatus();
     
     if (!member.permissions.has("ADMINISTRATOR") && (!status?.djRoleId || !member.roles.cache.has(status.djRoleId))) {
         return interaction.reply(errorEmbed("You need to be an administrator or DJ to use this command!"));
